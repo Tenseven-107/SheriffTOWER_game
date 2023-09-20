84f5a5e25e04b9dcfddc389b76132e86ffc23aab
Tenseven-107 <nadrichem@tanous.nl>
2023-09-20 15:54:34 +0200

    Worked on some of the area detection

--- Assets/Scripts/Enviroment/Game_elements/Towers/TowerDetection.cs
+++ Assets/Scripts/Enviroment/Game_elements/Towers/TowerDetection.cs
@@ -1,21 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TowerDetection : MonoBehaviour
 {
-    [SerializeField] float areaSize = 1f;
+    [SerializeField] float areaSize = 2.5f;
 
-    public enum Ranges { CLOSEST, FURTHEST };
-    [SerializeField] public Ranges range = Ranges.CLOSEST;
+    enum Ranges { CLOSEST, FURTHEST };
+    [SerializeField] Ranges range = Ranges.CLOSEST;
 
-    List<GameObject> enemies;
+    [SerializeField] bool testing;
+
     public GameObject lockedEnemy;
 
 
-    private void OnTriggerStay2D(Collider2D collision)
+    private void Update()
     {
-        
+        CheckArea();
     }
 
+
+    void CheckArea()
+    {
+        Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, areaSize);
+
+        if (hits.Length > 0 )
+        {
+            List<float> distances = new List<float>();
+
+            foreach(Collider2D hit in hits)
+            {
+                float distance = Vector2.Distance(transform.position, hit.transform.position);
+                distances.Add(distance);
+            }
+
+            float selectedDistance;
+            if (range == Ranges.CLOSEST)
+            {
+                selectedDistance = distances.Min();
+            }
+            else
+            {
+                selectedDistance = distances.Max();
+            }
+        }
+    }
+
+    void SetLock(float distance)
+    {
+
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        if (testing == true) Gizmos.DrawWireSphere(transform.position, areaSize);
+    }
 }
